81ef653235bfcf24a597a7922dd8151aea4e5bcb
washingtown <sharp-shoot2@126.com>
2021-07-25 16:35:15 +0800

    增加了webpack进度条和路径别名

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,23 @@
-const CracoLessPlugin = require('craco-less');
-
+const CracoAntDesignPlugin = require("craco-antd");
+const WebpackBar = require("webpackbar");
+const CracoAlias = require("craco-alias");
 module.exports = {
+  webpack: {
+    plugins: [
+      new WebpackBar({name:"Renderer", profile: true }),
+    ],
+  },
   plugins: [
     {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            javascriptEnabled: true,
-          },
-        },
-      },
+      plugin: CracoAntDesignPlugin
     },
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        baseUrl: ".",
+        tsConfigPath: "./tsconfig.alias.json"
+      }
+    }
   ],
 };
